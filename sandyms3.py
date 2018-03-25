4850c13b191d25e7c520313dfdfe1059b3735822
MSanthiya <37657240+MSanthiya@users.noreply.github.com>
2018-03-25 21:24:16 +0530

    Create sandyms3.py

---
+++ sandyms3.py
@@ -0,0 +1,7 @@
+char=(input("enter the character:"))
+char=a,e,i,o,u,A,E,I,O,U
+if(char=='a' or char=='e' or char=='i' or char=='o' or char=='u' or char=='A' or char=='E' or char=='I'or char=='O' or char=='U'):
+  print('it is a vowel letter')
+else:
+    print('it is a consonant letter')
+    
